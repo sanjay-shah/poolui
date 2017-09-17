c2ec71ff2cb27f7ae05c3ca0d74be447b61f81e1
Sanjay Shah <sanjay@strongloop.com>
2017-09-17 00:49:22 -0700

    change pool name

--- app/globals.default.js
+++ app/globals.default.js
@@ -4,7 +4,7 @@ angular.module('pool.globals', [])
 
 .factory('GLOBALS', function() {
 	return {
-		pool_name: "XMRPool.net",
+		pool_name: "monero.miningpool.io",
 		api_url : 'https://api.xmrpool.net',
 		api_refresh_interval: 5000,
 		app_update_interval: 30*60000
